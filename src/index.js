6ae25e1bac721cc0649003edd05cf86ac241b921
Ayush Indoria <96787650+akkii03@users.noreply.github.com>
2022-08-09 23:34:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,17 +9,20 @@ const myFileWriter = async (fileName, fileContent) => {
 const myFileReader = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	fileReader(fileName);
 }
 
 
 const myFileUpdater = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	fileUpdater(fileName,fileContent);
 }
 
 const myFileDeleter = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	fileDeleter(fileName);
 }
 
 
